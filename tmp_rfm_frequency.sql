22c4a4eaf41c6f67eedc3c2509a29e2ba96e2d3e
Matthew Suprunov <117388645+MatthewS-M@users.noreply.github.com>
2023-08-25 11:53:20 +0300

    Update tmp_rfm_frequency.sql

--- tmp_rfm_frequency.sql
+++ tmp_rfm_frequency.sql
@@ -1,1 +1,18 @@
+CREATE TABLE analysis.tmp_rfm_frequency (
+ user_id INT NOT NULL PRIMARY KEY,
+ frequency INT NOT NULL CHECK(frequency >= 1 AND frequency <= 5)
+);
 
+with order_count as (
+    select user_id, coalesce(count(*),0) as counter
+    from analysis.orders o 
+    where o.status  = 4
+    group by user_id
+)
+insert into analysis.tmp_rfm_frequency
+select o.user_id, ntile(5) over(order by counter) as frequency
+from analysis.orders o 
+    left join order_count oc using(user_id)
+where
+    o.order_ts > '2022-01-01'
+group by user_id, counter;
