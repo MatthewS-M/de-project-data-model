c29a548f32bc6b822c3f1aec560fee27b4e1d4cc
Matthew Suprunov <117388645+MatthewS-M@users.noreply.github.com>
2023-08-25 08:51:59 +0300

    Update orders_view.sql

--- orders_view.sql
+++ orders_view.sql
@@ -1,10 +1,10 @@
 create or replace view analysis.orders as
 with order_statuses as (
-  select order_id, status_id, dttm,
+  select order_id, status_id as new_status, dttm,
          row_number() over (partition by order_id order by dttm desc) as rn
   from production.orderstatuslog
 )
-select o.order_id, o.order_ts, o.user_id, o.bonus_payment, o.payment, o.cost, o.bonus_grant, os.status
+select o.order_id, o.order_ts, o.user_id, o.bonus_payment, o.payment, o.cost, o.bonus_grant, new_status
 from production.orders o
 join order_statuses os on o.order_id = os.order_id
 where os.rn = 1;
@@ -13,11 +13,11 @@ where os.rn = 1;
 
 create or replace view analysis.orders as
 with order_statuses as (
-  select order_id, status_id, dttm,
+  select order_id, status_id as new_status, dttm,
          rank() over (partition by order_id order by dttm desc) as rnk
   from production.orderstatuslog
 )
-select o.order_id, o.order_ts, o.user_id, o.bonus_payment, o.payment, o.cost, o.bonus_grant, os.status
+select o.order_id, o.order_ts, o.user_id, o.bonus_payment, o.payment, o.cost, o.bonus_grant, new_status
 from production.orders o
 join order_statuses os on o.order_id = os.order_id
 where os.rnk = 1;
